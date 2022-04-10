179a5ffa8e8d5a1630591069ca6aee065740668e
Aditya-Singh-rauthan <aditya126461@gmail.com>
2022-04-10 18:45:05 +0530

    db uri added

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-const db_uri = process.env.DB_URI
 
+const db_uri = 'mongodb+srv://aditya12:%40d!+y%4012@cluster0.1zjsj.mongodb.net/notes-safe-online?retryWrites=true&w=majority'
 const connectDatabase = () =>{
+    // console.log(">>>>>>process.env",process.env)
     mongoose.connect(db_uri,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
         console.log('>>>>connected with mongo',data.connection.host)
     }).catch((err)=>{
